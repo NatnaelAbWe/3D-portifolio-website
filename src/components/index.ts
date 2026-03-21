bec32cb6a7e97a1641c7e90ae00ebd5e41feffce
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-03-21 16:43:39 +0300

    feat: fully responsive work section

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,7 +9,7 @@ import { Navbar } from "./Navbar";
 import About from "./About";
 import Tech from "./Tech";
 import Experiance from "./Experiance";
-import { Work } from "./Work";
+import Work from "./Work";
 import { Feedbacks } from "./Feedbacks";
 import { Contact } from "./Contact";
 import { CanvasLoader } from "./Loader";
